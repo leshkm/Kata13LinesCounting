14af1585ba069e428f7f3a89d00c1f1b9951d8f1
Oleksii Mohylin <alxey.mogilin@gmail.com>
2020-07-07 21:51:23 +0300

    project stub

---
+++ src/main/java/ua/mohylin/kata13/JavaCodeLinesCountingApplication.java
@@ -0,0 +1,9 @@
+package ua.mohylin.kata13;
+
+public class JavaCodeLinesCountingApplication {
+
+    public static void main(String[] args) {
+        System.out.println("This is a stub");
+    }
+
+}
